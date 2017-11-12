bca13282a0540fd35c0daa9cfc1028e564a98fde
abc1196 <alejob_11@hotmail.com>
2017-11-12 14:32:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/apps_miocali_project/control/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.apps_miocali_project.control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.apps_miocali_project.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
