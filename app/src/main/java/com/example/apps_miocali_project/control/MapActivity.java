84f99743fab4d75447423b1bb6365a2bce8f6392
abc1196 <alejob_11@hotmail.com>
2017-11-13 20:17:50 -0500

    Second View commit

--- app/src/main/java/com/example/apps_miocali_project/control/MapActivity.java
+++ app/src/main/java/com/example/apps_miocali_project/control/MapActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -34,7 +35,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney, Australia, and move the camera.
         LatLng sydney = new LatLng(3.39948, -76.53147);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Home"));
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Home").icon(BitmapDescriptorFactory.fromResource(R.drawable.rsz_wifi_map)));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(3.39948 ,-76.53147),16));
     }
 }
