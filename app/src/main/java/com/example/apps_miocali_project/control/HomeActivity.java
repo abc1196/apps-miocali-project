43abdec076d7dcb3748b7ed376a6eff026605492
abc1196 <alejob_11@hotmail.com>
2017-11-13 19:33:12 -0500

    Initial View commit

--- app/src/main/java/com/example/apps_miocali_project/control/HomeActivity.java
+++ app/src/main/java/com/example/apps_miocali_project/control/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.example.apps_miocali_project.control;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +14,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(getActivity(),MapActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+    public Activity getActivity(){
+        return this;
     }
 }
