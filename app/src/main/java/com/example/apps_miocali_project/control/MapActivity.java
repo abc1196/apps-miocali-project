9b6596766f8db32bb597c09f837912c6908f81aa
abc1196 <alejob_11@hotmail.com>
2017-11-26 23:05:56 -0500

    Filter and Location commit

--- app/src/main/java/com/example/apps_miocali_project/control/MapActivity.java
+++ app/src/main/java/com/example/apps_miocali_project/control/MapActivity.java
@@ -1,7 +1,12 @@
 package com.example.apps_miocali_project.control;
 
+import com.crystal.crystalrangeseekbar.interfaces.OnSeekbarChangeListener;
+import com.crystal.crystalrangeseekbar.widgets.CrystalSeekbar;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,9 +14,14 @@ import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.apps_miocali_project.R;
 
@@ -25,16 +35,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.w3c.dom.Text;
+
 import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     SupportMapFragment mapFragment;
+    android.support.design.widget.FloatingActionButton fabUbicacion;
     FloatingActionButton fabParadas, fabRecargas, fabWifi;
     boolean paradas,recargas,wifi;
     private DataBase db;
     private GoogleMap map;
+    private float distanciaFiltro;
 
     private ArrayList<Marker> listParadas;
     private ArrayList<Marker> listRecargas;
@@ -45,10 +59,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        distanciaFiltro=500;
         db= new DataBase(this);
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        fabUbicacion=(android.support.design.widget.FloatingActionButton)findViewById(R.id.fabUbicacion);
         fabParadas=(FloatingActionButton) findViewById(R.id.accion_paradas);
         paradas=false;
         listParadas= new ArrayList<Marker>();
@@ -149,6 +165,47 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             fabWifi.getBackground().setColorFilter(ContextCompat.getColor(this, R.color.colorDisabled), PorterDuff.Mode.MULTIPLY);
             wifi=false;
         }
+    }
+
+    public void darUbicacionActual(View v){
+
+    }
+
+    public void activarMenuFiltro(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("Ajustes de Filtro");
+        builder.setMessage("Establece radio máximo");
+        LayoutInflater inflater = this.getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_filter_settings, null);
+        final TextView txtDistancia= (TextView) view.findViewById(R.id.txtDistancia);
+        final CrystalSeekbar seekbar=(CrystalSeekbar)view.findViewById(R.id.rangeSeekbar4);
+        seekbar.setMinStartValue(distanciaFiltro ).apply();
+        seekbar.setOnSeekbarChangeListener(new OnSeekbarChangeListener() {
+            @Override
+            public void valueChanged(Number value) {
+                distanciaFiltro=value.floatValue();
+                txtDistancia.setText(String.valueOf(value) + " m");
+            }
+        });
+        builder.setView(view);
+        builder.setPositiveButton("Aceptar",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //TODO BENRU
+                        dialog.cancel();
+                    }
+                });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 
