9b89a46d13474821baca8d4b5e551dd344ba2657
Nicolas <nikoremi97@hotmail.com>
2017-11-28 21:43:34 -0500

    COMMIT

--- app/src/main/java/com/example/apps_miocali_project/control/ParadaActivity.java
+++ app/src/main/java/com/example/apps_miocali_project/control/ParadaActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -25,6 +26,7 @@ public class ParadaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_parada);
         db=new DataBase(getApplicationContext());
         idParada = getIntent().getStringExtra("parada");
+        Log.d("ID_PARADA",idParada);
         rutasParada=new ArrayList<>();
         rutasParada=db.cargarRutasParada(idParada);
 
