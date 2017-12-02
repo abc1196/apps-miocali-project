146c147fe513d98f28f8b75b11b41c71906c5c82
Juan Camilo Swan <juanswan13@hotmail.com>
2017-12-02 01:01:27 -0500

    Merge branch master y developSwan.

--- app/src/main/java/com/example/apps_miocali_project/control/ParadaWidget.java
+++ app/src/main/java/com/example/apps_miocali_project/control/ParadaWidget.java
@@ -32,11 +32,8 @@ public class ParadaWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
             int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.parada_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
