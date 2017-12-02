b36baae81b937a1de845213f340f4399222e58a2
Nicolas <nikoremi97@hotmail.com>
2017-12-02 16:18:17 -0500

    el buen commit

--- app/src/main/java/com/example/apps_miocali_project/control/ConexionHTTPPRuta.java
+++ app/src/main/java/com/example/apps_miocali_project/control/ConexionHTTPPRuta.java
@@ -1,15 +1,11 @@
 package com.example.apps_miocali_project.control;
 
-import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.PasswordAuthentication;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 
